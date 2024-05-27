7800986fb9435df1d5cf6cf4b42849c00e7f4d2a
anishmendes <anishmainali31@gmail.com>
2024-05-27 12:17:20 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div>
+      HI iam anish
+    </div>
+  )
+}
+
+export default App
+
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom': '11px 19px 34px -9px rgba(0,95,163,0.6)',
+      },
+    },
+  },
+  plugins: [],
+}
