aaa4df1f009398a61dfb8e2e10e4160381b6f3d1
anishmendes <anishmainali31@gmail.com>
2024-05-27 15:22:21 +0545

    all components done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-   <div className='flex flex-row bg-blue-400 h-[150px] overflow-hidden'>
-      <img src="./src/assets/logo.png" alt="logo"    className='max-h-[80px] mt-[50px]  '/>
+   <div className='flex flex-col bg-white h-[150px] overflow-hidden'>
+      {/* <img src="./src/assets/logo.png" alt="logo"    className='max-h-[80px] mt-[50px] w-[100px] '/> */}
     <div className='flex flex-row justify-between pl-[180px] pt-[30px]'>
-    <span className='pr-[260px]'>Kathmandu,Nepal</span>
-      <span className='pr-[500px]'>guankoachar@gmail.com</span>
+    <span className='pr-[260px] text-cyan-600 font-bold '>Kathmandu,Nepal</span>
+      <span className='pr-[500px] text-cyan-600 font-bold'>guankoachar@gmail.com</span>
         
-        <span className=''>+977 9876000000</span>
+        <span className='text-cyan-600  font-bold pr-[80px]'>+977 9876000000</span>
     </div>
-
+    <div className='bg-cyan-600 flex flex-row  pl-[60px]'>
+      <a href="" >Home</a>
+      <a href="">Home</a>
+      <a href="">Home</a>
+      <a href="">Home</a>
+    </div>
+      
    </div>
   )
 }
