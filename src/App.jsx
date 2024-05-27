89c2603e485a28485e72b998136298c675026e9d
anishmendes <anishmainali31@gmail.com>
2024-05-27 13:02:06 +0545

    all components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer';
 
 const App = () => {
   return (
-    <div>
-      HI iam anish
-    </div> 
+   <>
+     <div className='w-full min-h-screen overflow-hidden'>
+        <Navbar />
+        {/* <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path="/Admission" element={<Admission />} />
+          <Route path='/Faculty' element={<Faculty />} />
+          <Route path='/Eca' element={<Eca />} />
+          <Route path='/Signup' element={<SignUp />} />
+          <Route path='/Notice' element={<Notice />} />
+          <Route path='/About' element={<About />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes> */}
+        <Footer />
+      </div>
+   </>
   )
 }
 
---
+++ src/Components/Pages/Ourstory.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ourstory = () => {
+  return (
+    <div>
+      OurStory
+    </div>
+  )
+}
+
+export default Ourstory
