3a23ad9bc66a87591258c98cbc5d81b1855ba47a
anishmendes <anishmainali31@gmail.com>
2024-05-27 13:44:55 +0545

    chnages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      This is Navabar
+   <div className='flex flex-row bg-blue-400 h-[150px] overflow-hidden'>
+      <img src="./src/assets/logo.png" alt="logo"    className='max-h-[80px] mt-[50px]  '/>
+    <div className='flex flex-row justify-between pl-[180px] pt-[30px]'>
+    <span className='pr-[260px]'>Kathmandu,Nepal</span>
+      <span className='pr-[500px]'>guankoachar@gmail.com</span>
+        
+        <span className=''>+977 9876000000</span>
     </div>
+
+   </div>
   )
 }
 
