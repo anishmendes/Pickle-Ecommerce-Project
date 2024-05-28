40e13b76cba5abafb6ddf313d13182ce967ebd52
anishmendes <anishmainali31@gmail.com>
2024-05-28 10:11:43 +0545

    ...

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
-   <div className='flex flex-col bg-white h-[150px] overflow-hidden'>
-      {/* <img src="./src/assets/logo.png" alt="logo"    className='max-h-[80px] mt-[50px] w-[100px] '/> */}
+   <div className='flex flex-col bg-white  overflow-hidden'>
     <div className='flex flex-row justify-between pl-[180px] pt-[30px]'>
-    <span className='pr-[260px] text-cyan-600 font-bold '>Kathmandu,Nepal</span>
-      <span className='pr-[500px] text-cyan-600 font-bold'>guankoachar@gmail.com</span>
+    <span className=' text-cyan-600 font-bold '>Kathmandu,Nepal</span>
+      <span className=' text-cyan-600 font-bold'>guankoachar@gmail.com</span>
         
         <span className='text-cyan-600  font-bold pr-[80px]'>+977 9876000000</span>
     </div>
-    <div className='bg-cyan-600 flex flex-row  pl-[60px]'>
-      <a href="" >Home</a>
-      <a href="">Home</a>
-      <a href="">Home</a>
-      <a href="">Home</a>
-    </div>
-      
+      <img src="./src/assets/logo.png" alt="logo"    className='max-h-[80px] w-[100px] '/>
+  
+      <div className='bg-cyan-400 flex flex-row items-center justify-around pt-4 pb-8' >
+         <a href="/">Home</a>
+         <a href="/">Home</a>
+         <a href="/">Home</a>
+         <a href="/">Home</a>
+         <a href="/">Home</a>
+      </div>
    </div>
   )
 }
