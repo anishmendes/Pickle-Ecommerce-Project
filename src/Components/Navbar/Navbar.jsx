780e1093c1ea410d8aff4198de8f8c27e04523cf
anishmendes <anishmainali31@gmail.com>
2024-05-28 10:19:49 +0545

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,20 +5,22 @@ import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
    <div className='flex flex-col bg-white  overflow-hidden'>
-    <div className='flex flex-row justify-between pl-[180px] pt-[30px]'>
+    <div className='flex flex-row justify-around pl-[180px] pt-[30px]'>
     <span className=' text-cyan-600 font-bold '>Kathmandu,Nepal</span>
       <span className=' text-cyan-600 font-bold'>guankoachar@gmail.com</span>
         
         <span className='text-cyan-600  font-bold pr-[80px]'>+977 9876000000</span>
     </div>
       <img src="./src/assets/logo.png" alt="logo"    className='max-h-[80px] w-[100px] '/>
-  
-      <div className='bg-cyan-400 flex flex-row items-center justify-around pt-4 pb-8' >
+
+      <div className="flex justify-end ">
+      <div className='bg-cyan-400  flex justify-evenly gap-[100px] mr-[60px] pt-4 pb-8' >
          <a href="/">Home</a>
-         <a href="/">Home</a>
-         <a href="/">Home</a>
-         <a href="/">Home</a>
-         <a href="/">Home</a>
+         <a href="/">Our Story</a>
+         <a href="/">Shop</a>
+         <a href="/">Contact Us </a>
+         <a href="/">Checkout</a>
+      </div>
       </div>
    </div>
   )
