f24325ff79f30049303f14c1f8e06e989b118aaa
anishmendes <anishmainali31@gmail.com>
2024-05-28 14:24:46 +0545

    4th section also complete

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -21,6 +21,27 @@ const Home = () => {
     </div>
         <Wrapper/>
         <Fresh/>
+
+        {/* 15% PAGE */}
+        <div className='flex flex-row items-center justify-between bg-slate-300 mt-[100px]'>
+        
+
+
+
+          <div className='flex flex-col items-center pl-[100px]  pb-[80px] pt-[80px] max-w-[500px] ' >
+            <h1 className='text-2xl font-bold pb-4'>100% Organic pickle in our store</h1>
+            <p className='text-xl  pb-6'>Handcrafted with locally sourced ingredients, our 100% organic pickle delivers authentic flavors while promoting sustainability and environmental consciousness.</p>
+            <div className='flex flex-row gap-0 pl-[100px] items-center  '>
+            <input type='text' placeholder='Name of the pickle' className='p-3 w-[550px] rounded-xl text-black' />
+           <button className='bg-customGray text-white  border-green-500 border-2 rounded-xl p-2 pl-6 pr-6 '>Search</button>
+          </div>
+          </div>
+
+          <div className='flex items-end rounded pr-[50px] pt-8 pb-10'>
+            <img src="./src/assets/pickle.png" alt="pickle" className='h-[350px] w-[550px] ' />
+          </div>
+
+        </div>
     </div>
   )
 }
