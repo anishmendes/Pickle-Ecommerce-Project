b61d1f40cf8d3da47a4ee34947aa1d8f578fb897
anishmendes <anishmainali31@gmail.com>
2024-05-28 16:16:35 +0545

    testimonails data added

---
+++ src/Components/Testimonal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+
+const Testimonal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Testimonal
--- src/Data.js
+++ src/Data.js
@@ -21,3 +21,48 @@ export const hwrapper = [
       title: "Support Everytime",
     },
   ]
+  export const testimonal = [
+    {
+      id: 1,
+      name: "Saurav Poudel",
+     
+      desc: "Absolutely delicious! The perfect mix of tangy and spicy. Love that it's organic!",
+      cover: "./images/testo/t1.webp",
+    },
+    {
+      id: 2,
+      name: "Prayush Niroula",
+      
+      desc: "Great flavor and very fresh. Delivery took a bit long, but worth the wait!",
+      cover: "./images/testo/t2.webp",
+    },
+    {
+      id: 3,
+      name: "Rabin Belbase",
+    
+      desc: "Authentic taste, just like homemade. Packaging could be more eco-friendly.",
+      cover: "./images/testo/t3.webp",
+    },
+    {
+      id: 4,
+      name: "Anish Mainali",
+   
+      desc: "Good pickles, but a bit too salty for me. Still, very fresh and crunchy.",
+      cover: "./images/testo/t4",
+    },
+    {
+      id: 5,
+      name: "Aditya Shrestha",
+ 
+      desc: "Best pickles I've ever had! Organic and full of flavor. Highly recommend!",
+      cover: "./images/testo/t5",
+    },
+    {
+      id: 6,
+      name: "Pragati Dahal",
+    
+      desc: "Tasty but a bit too vinegary for my liking. Love the organic ingredients though",
+      cover: "./images/testo/t6",
+    },
+   
+  ]
