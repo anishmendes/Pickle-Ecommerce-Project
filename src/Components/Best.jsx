619703d321b6601904d32d03f6c9d78f7262fade
anishmendes <anishmainali31@gmail.com>
2024-05-28 15:41:21 +0545

    changes

---
+++ src/Components/Best.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+
+const best = [
+    {
+        cover: "./src/assets/akhabare_pickle.png",
+      name: "Akhabare Pickle",
+      
+    },
+    {
+        cover: "./src/assets/chicken_pickle.png",
+      name: "Chicken  Pickle",
+      
+    },
+    {
+        cover: "./src/assets/mutton_pickle.png",
+      name: "Mutton Pickle",
+      
+    },
+    {
+        cover: "./src/assets/chilli_pickle.png",
+      name: "Chilli Pickle",
+      
+    },
+
+const Best = () => {
+  return (
+    <>
+      <h1 className='text-2xl pt-[50px] flex items-center justify-center font-bold'>Best Selling Pickles</h1>
+    
+    </>
+  )
+}
+
+export default Best
