e6ac4a28ffd95dfa503e1e6f7ab3364e8b0d1dca
anishmendes <anishmainali31@gmail.com>
2024-05-29 09:55:36 +0545

    social media icons section done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -7,21 +9,25 @@ const Footer = () => {
       <h1 className='text-3xl font-bold pt-2 text-green-200 flex pr-10 '>GAUNKO AACHAR</h1>
          <input type='text' placeholder='Enter Your Email' className='p-3 w-[550px] rounded-xl text-black' />
          <button className='bg-customGray text-white  border-green-500 border-2 rounded-xl p-2 pl-6 pr-6  '>Join Now</button>
+
+         <div className='pl-[50px] flex items-center justify-around gap-6'>
+       <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faFacebookF} className="text-blue-200 hover:text-blue-800 text-3xl transition duration-300 ease-in-out " />
+      </a>
+      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faTwitter} className="text-blue-200 hover:text-cyan-500 text-3xl transition duration-300 ease-in-out" />
+      </a>
+      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faInstagram} className="text-blue-200 hover:text-red-600 text-3xl transition duration-300 ease-in-out" />
+      </a>
+      <a href="https://wa.me/yourphonenumber" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faWhatsapp} className="text-blue-200 hover:text-green-500 text-3xl transition duration-300 ease-in-out" />
+      </a>
+       </div>
       </div>
 
      
-       <div className=' pt-[50px]'>
-       <a href="https://www.facebook.com">
-    <i className='fab fa-facebook-f icon'></i>
-</a>
-<a href="https://twitter.com">
-    <i className='fab fa-twitter icon'></i>
-</a>
-<a href="https://www.instagram.com">
-    <i className='fab fa-instagram icon'></i>
-</a>
-     
-       </div>
+      
 
 
        <div className='flex flex-row pt-[20px] pb-[50px] justify-around  items-center'>
