9f5e0b641ffd9856f2576c2fdab247ef17e122d5
anishmendes <anishmainali31@gmail.com>
2024-06-02 19:44:39 +0545

    footer chnages

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ import { faFacebookF, faTwitter, faInstagram, faWhatsapp } from '@fortawesome/fr
 
 const Footer = () => {
   return (
-    <div className='p-auto bg-darkblue mt-[100px]'>
+    <div className='p-auto bg-hard mt-[100px]'>
       <div className='flex flex-row items-center justify-center  pt-[50px] gap-[1.5px] '>
-      <h1 className='text-3xl font-bold pt-2 text-green-200 flex pr-10 '>GAUNKO AACHAR</h1>
+      <h1 className='text-3xl font-bold pt-2 text-navbar1 flex pr-10 '>GAUNKO AACHAR</h1>
          <input type='text' placeholder='Enter Your Email' className='p-3 w-[550px] rounded-xl text-black' />
-         <button className='bg-customGray text-white  border-green-500 border-2 rounded-xl p-[10px] pl-6 pr-6  '>Join Now</button>
+         <button className='bg-customGray text-black  border-green-500 border-2 rounded-xl p-[10px] pl-6 pr-6  '>Join Now</button>
 
          <div className='pl-[50px] flex items-center justify-around gap-6'>
        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
@@ -34,12 +34,12 @@ const Footer = () => {
 
          <div className='flex flex-col max-w-[450px] '>
          
-            <h2 className='text-2xl font-medium text-white'>Why people like us ?</h2>
-            <p className='text-xl text-white pt-5'>People appreciate us because we offer organic pickles bursting with spices and tanginess. Our commitment to quality ingredients ensures a flavorful experience, satisfying the cravings of spice and sour lovers alike.</p>
+            <h2 className='text-2xl font-medium text-black'>Why people like us ?</h2>
+            <p className='text-xl text-black pt-5'>People appreciate us because we offer organic pickles bursting with spices and tanginess. Our commitment to quality ingredients ensures a flavorful experience, satisfying the cravings of spice and sour lovers alike.</p>
           </div> 
          <div className='flex flex-col'>
-          <h2 className='text-2xl font-semibold text-white'>Shop Info</h2>
-          <ul className='pt-8 text-xl text-white font-medium'>
+          <h2 className='text-2xl font-semibold text-black'>Shop Info</h2>
+          <ul className='pt-8 text-xl text-black font-medium'>
             <li className='pb-1'>Aboutus</li>
             <li className='pb-1'>Contact Us </li>
             <li className='pb-1'>Privacy Policy</li>
@@ -48,8 +48,8 @@ const Footer = () => {
           </ul>
           </div> 
          <div className='flex flex-col pb-[65px] '>
-         <h2 className='text-2xl font-semibold text-white '>Account</h2>
-          <ul className='pt-8  text-xl text-white font-medium '>
+         <h2 className='text-2xl font-semibold text-black '>Account</h2>
+          <ul className='pt-8  text-xl text-black font-medium '>
            <li className='pb-1'> My Account</li>
             <li className='pb-1'>Shop Details </li>
             <li className='pb-1'>Shopping Cart</li>
@@ -60,8 +60,8 @@ const Footer = () => {
          
 
 
-         <h2 className='text-2xl font-semibold text-white'>Contact</h2>
-          <ul className='pt-8 text-xl text-white font-medium'>
+         <h2 className='text-2xl font-semibold text-black'>Contact</h2>
+          <ul className='pt-8 text-xl text-black font-medium'>
            <li className='pb-1'>  Address: New Baneshwor, Kathmandu 44600</li>
             <li className='pb-1'>Email: gaunnko@gmail.com </li>
             <li className='pb-1'>Phone: (+977) 9808284192</li>
