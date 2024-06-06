278fc106c51c09a3cbbc5fb136417ff43bdbd908
anishmendes <anishmainali31@gmail.com>
2024-06-06 16:08:01 +0545

    .....

--- src/Components/Best.jsx
+++ src/Components/Best.jsx
@@ -44,12 +44,16 @@ const Best = () => {
            whileHover={{ scale: 1.1, y: -10, backgroundColor: 'rgb(253, 228, 158)', boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.3)' }}  // Define the hover effect
            whileInView={{ opacity: 1, scale: 1, x: 0 }}  // Animate when the component is in view
            viewport={{ once: true, amount: 0.5 }}
+           whileTap={{ scale: 0.9, transition: { duration: 0.3 } }}
            className="flex items-center bg-navbar justify-around flex-col mt-[50px] max-w-[400px]   max-h-[650px] shadow-2xl" >
              <div  >
                <img src={val.cover} alt='img'  className='flex max-w-[300px] max-h-[300px]'/>
              </div>
              <div className="pt-6 pb-6 p-7">
-               <h1 className="text-xl font-bold text-white bg-navbar1 cursor-pointer  hover:bg-navbar1 hover:text-white   border border-hard border-[3px] rounded p-2 ">{val.name}</h1>
+               <h1
+               
+               
+               className="text-xl font-bold text-white bg-navbar1 cursor-pointer  hover:bg-navbar1 hover:text-white   border border-hard border-[3px] rounded p-2 ">{val.name}</h1>
               
              </div>
      
