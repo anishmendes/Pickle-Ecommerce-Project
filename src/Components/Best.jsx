5fea4ea1b7c4d4b124cac2968ab403af98087f78
anishmendes <anishmainali31@gmail.com>
2024-06-06 16:03:31 +0545

    chnages

--- src/Components/Best.jsx
+++ src/Components/Best.jsx
@@ -41,15 +41,15 @@ const Best = () => {
            initial={{ opacity: 0, scale: 0.3, x: -100 }}  // Start from off-screen to the left
            animate={{ opacity: 1, scale: 1, x: 0 }}  // Animate to its final position
            transition={{ duration: 1 }}  // Control the duration of the initial animation
-           whileHover={{ scale: 1.1, y: -10, backgroundColor: 'rgb(254, 185, 65)', boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.3)' }}  // Define the hover effect
+           whileHover={{ scale: 1.1, y: -10, backgroundColor: 'rgb(253, 228, 158)', boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.3)' }}  // Define the hover effect
            whileInView={{ opacity: 1, scale: 1, x: 0 }}  // Animate when the component is in view
            viewport={{ once: true, amount: 0.5 }}
-           className="flex items-center bg-navbar1 justify-around flex-col mt-[50px] max-w-[400px]   max-h-[650px] shadow-2xl" >
+           className="flex items-center bg-navbar justify-around flex-col mt-[50px] max-w-[400px]   max-h-[650px] shadow-2xl" >
              <div  >
                <img src={val.cover} alt='img'  className='flex max-w-[300px] max-h-[300px]'/>
              </div>
              <div className="pt-6 pb-6 p-7">
-               <h1 className="text-xl font-bold text-white bg-hard cursor-pointer  hover:bg-navbar1 hover:text-white   border border-hard border-[3px] rounded p-2 ">{val.name}</h1>
+               <h1 className="text-xl font-bold text-white bg-navbar1 cursor-pointer  hover:bg-navbar1 hover:text-white   border border-hard border-[3px] rounded p-2 ">{val.name}</h1>
               
              </div>
      
