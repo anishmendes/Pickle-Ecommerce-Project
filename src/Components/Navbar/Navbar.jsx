aebac65d03c8c1406c32e55dec8902ebef2c5249
anishmendes <anishmainali31@gmail.com>
2024-06-07 17:44:36 +0545

    navbar motion added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {  NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { motion } from 'framer-motion';
 import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
@@ -26,11 +26,46 @@ const Navbar = () => {
         </button>
       </div>
       <div className={`bg-navbar1 mt-[10px] rounded-[20px] text-green-800 font-semibold  pl-[50px] pr-[50px] md:flex flex-col md:flex-row justify-evenly md:gap-[100px] gap-4 md:mr-[60px] mr-4 pt-8 pb-8 w-full md:w-auto ${isOpen ? 'flex' : 'hidden'} md:flex`}>
-        <NavLink to="/" className="text-center">Home</NavLink>
-        <NavLink to="/Shop" className="text-center">Shop</NavLink>
-        <NavLink to="/Ourstory" className="text-center">Ourstory</NavLink>
-        <NavLink to="/Contact" className="text-center">Contact</NavLink>
-        <NavLink to="/Checkout" className="text-center">Checkout</NavLink>
+      <motion.div
+        whileHover={{ scale: 1.1, color: '#FDE49E' }}
+        whileTap={{ scale: 0.9,  transition: { duration: 0.3 } }}
+      >
+        <NavLink to="/" className="text-center">
+          Home
+        </NavLink>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1, color: '#FDE49E' }}
+        whileTap={{ scale: 0.9,  transition: { duration: 0.3 } }}
+      >
+        <NavLink to="/Shop" className="text-center">
+          Shop
+        </NavLink>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1, color: '#FDE49E' }}
+        whileTap={{ scale: 0.9,  transition: { duration: 0.3 } }}
+      >
+        <NavLink to="/Ourstory" className="text-center">
+          Ourstory
+        </NavLink>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1, color: '#FDE49E' }}
+        whileTap={{ scale: 0.9,  transition: { duration: 0.3 } }}
+      >
+        <NavLink to="/Contact" className="text-center">
+          Contact
+        </NavLink>
+      </motion.div>
+      <motion.div
+        whileHover={{ scale: 1.1, color: '#FDE49E' }}
+        whileTap={{ scale: 0.9, transition: { duration: 0.3 } }}
+      >
+        <NavLink to="/Checkout" className="text-center">
+          Checkout
+        </NavLink>
+      </motion.div>
      
       <div className='flex flex-wrap gap-[50px] pl-[50px]'>
       <FontAwesomeIcon icon={faSearch} className="text-2xl text-white" />
