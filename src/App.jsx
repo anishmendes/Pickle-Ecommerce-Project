62a5cdc8d24d6b90f49f1774225362da6ac3af39
anishmendes <anishmainali31@gmail.com>
2024-06-10 12:04:18 +0545

    app provider and context

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,29 @@ import Contact from './Components/Pages/Contact';
 import Checkout from './Components/Pages/Checkout';
 import Cart from './Components/Pages/Cart';
 import SingleProduct from './Components/Pages/SingleProduct';
-import {  Routes, Route, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-   <>
+   <Router>
    
      <div className='w-full min-h-screen overflow-hidden'>
         <Navbar />
         <Routes>
-            <Route path='/' exact element={<Home/>} />
+            <Route path='/'  element={<Home/>} />
             <Route path='/Shop'  element={<Shop/>} />
             <Route path='/Ourstory'  element={<Ourstory/>} />
             <Route path='/Contact'  element={<Contact/>} />
             <Route path='/Checkout'  element={<Checkout/>} />
             <Route path='/Cart' element={<Cart/>}/>
             <Route path='/singleproduct/:id' element={<SingleProduct/>}/>
-            <Route path='*' element={<Navigate to='/' />} />
+            {/* <Route path='*' element={<Navigate to='/' />} /> */}
             
         </Routes>
         
         <Footer />
       </div>
-   </>
+   </Router>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { AppProvider } from './context/productcontext';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <Router>
+    <AppProvider>
 
     <App />
-    </Router>
-  </React.StrictMode>,
-)
+    </AppProvider>
+  </React.StrictMode>
+);
