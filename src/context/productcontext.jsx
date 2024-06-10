cde69d9c03eeeac5ca7d940ea6c4bfe32c15ab21
anishmendes <anishmainali31@gmail.com>
2024-06-10 15:43:27 +0545

    data connection also done using fetch api

--- src/context/productcontext.jsx
+++ src/context/productcontext.jsx
@@ -1,8 +1,35 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+// import axios from "axios"
+
 
 const AppContext = createContext();
 
+const API = "http://localhost:3000/product";
+
 const AppProvider = ({ children }) => {
+    const [products, setProducts] = useState([]);
+
+    const getProducts = async () => {
+        try {
+            const response = await fetch(API);
+            if (!response.ok) {
+                throw new Error('Failed to fetch products');
+            }
+            const data = await response.json();
+            setProducts(data);
+            console.log("Products:", data);
+        } catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    };
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+
+
+
     return <AppContext.Provider value= {{ myName:"anish mainali"}}>
         {children}
     </AppContext.Provider>
